0a241f475d54b83ee5ce73f450d64f9c6daf9ea5
Muhammad <mhmrofficial@gmail.com>
2023-10-21 05:02:16 +0700

    fix: update code

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-  import { useAsync } from "~/composables/api";
-  import overview from "../templates/overview/index.vue"
-
+import { useQuery } from "~/composables/api";
+import overview from "../templates/overview/index.vue";
 </script>
 
 <template>
   <div>
-    <overview/>
+    <overview />
   </div>
 </template>
--- templates/overview/index.vue
+++ templates/overview/index.vue
@@ -1,30 +1,15 @@
 <script setup lang="ts">
-  import { Form, Field } from "vee-validate"
-  import Input from '~/components/forms/input/input.vue';
-  import { useCounterStore } from "~/stores/counter";
-  import { createHmac,  } from "crypto";
-  import { onMounted } from "vue";
+import { Form, Field } from "vee-validate";
+import Input from "~/components/forms/input/input.vue";
+import { useCounterStore } from "~/stores/counter";
+const count2 = reactive({ count: 0 });
 
-  onMounted(() => {
+const { data, onChange } = useCustom();
+const counter = useCounterStore();
 
-    const hash = createHmac("sha256", "123")
-      .update("test")
-      .digest("base64")
-  
-    console.log(hash)
-  })
-
-
-  const count = ref(0)
-  const count2 = reactive({count: 0})
-
-  const { data, onChange } = useCustom()
-  const counter = useCounterStore();
-
-  const handleSubmit = (e: any) => {
-    console.log("---", e) 
-  }
-
+const handleSubmit = (e: any) => {
+  console.log("---", e);
+};
 </script>
 
 <template>
@@ -34,11 +19,16 @@
       {{ data.loading ? "True" : "False" }}
       <button @click="() => onChange(!data.loading)">click</button>
       <Form @submit="handleSubmit">
-        <Input name="email"/>
+        <Input name="email" />
         <button>submit</button>
       </Form>
       <p class="mt-4 text-gray-400">The Nuxt.js boilerplate</p>
-      <button class="bg-pink-600 py-2 px-4 rounded mt-4" @click="counter.increment">Click {{ counter.count }}</button>
+      <button
+        class="bg-pink-600 py-2 px-4 rounded mt-4"
+        @click="counter.increment"
+      >
+        Click {{ counter.count }}
+      </button>
     </div>
   </div>
 </template>
