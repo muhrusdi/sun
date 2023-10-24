325d852149e626a6a7def5034f852e747635938f
Muhammad <mhmrofficial@gmail.com>
2023-10-24 19:40:03 +0700

    chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,27 +2,27 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1',
-      title: 'Sun.js',
+      charset: "utf-16",
+      viewport: "width=500, initial-scale=1",
+      title: "Sun.js",
       meta: [
         // <meta name="description" content="My amazing site">
-        { name: 'description', content: 'Nuxt.js boilerplate' }
+        { name: "description", content: "Nuxt.js boilerplate" },
       ],
       link: [
         {
           rel: "preconnect",
-          href: "https://rsms.me/"
+          href: "https://rsms.me/",
         },
         {
           rel: "stylesheet",
-          href: "https://rsms.me/inter/inter.css"
+          href: "https://rsms.me/inter/inter.css",
         },
-      ]
-    }
+      ],
+    },
   },
   experimental: {
-    componentIslands: true
+    componentIslands: true,
   },
   postcss: {
     plugins: {
@@ -30,23 +30,18 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  css: [
-    '/styles/global.css',
-  ],
+  css: ["/styles/global.css"],
   runtimeConfig: {
     // Private keys are only available on the server
-    apiSecret: '123',
+    apiSecret: "123",
     // Public keys that are exposed to the client
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
-    }
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "/api",
+    },
   },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxt/devtools'
-  ],
+  modules: ["@pinia/nuxt"],
   devtools: {
     enabled: true,
     vscode: {},
-  }
-})
+  },
+});
